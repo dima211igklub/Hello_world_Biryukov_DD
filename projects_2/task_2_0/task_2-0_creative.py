2691b400989a502af7d3457a6a539a8c178ab1f4
dima211igklub <74669831+dima211igklub@users.noreply.github.com>
2026-02-19 14:57:20 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2-0_creative.py
@@ -0,0 +1,8 @@
+
+print("\n" + "=" * 30)
+print("ПРИВЕТСТВУЮ".center(30))
+print("=" * 30)
+print("Студент 1 курса 2026".center(30))
+print("=" * 30)
+print("Удачи в учёбе!".center(30))
+print("=" * 30)
