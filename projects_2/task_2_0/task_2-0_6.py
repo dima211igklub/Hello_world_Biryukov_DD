acadf9990b81e46dfc43aa7862d87a21b673e8df
dima211igklub <74669831+dima211igklub@users.noreply.github.com>
2026-02-19 08:07:53 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,5 @@
+with open("C:/Users/dima2/OneDrive/Рабочий стол/Инфа/biruykov_dd/projects_2/task_2_0/output.txt", "w", encoding="utf-8") as f:
+    print("Бирюков Дмитрий Денисович", file=f)
+    print("СПБПУ", file=f)
+    print("Биотехнология", file=f)
+    print("4731901/50001", file=f)
